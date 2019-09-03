a24c63b4f5d5571d8b050d611d5940e6f7d910a6
Kajal <kajal11122000@gmail.com>
2019-09-03 19:23:12 +0530

    Create, update and delete view added

--- blog/models.py
+++ blog/models.py
@@ -10,3 +10,90 @@ class Post(models.Model):
 
     def __str__(self):
         return str(self.name)
+
+class AboutTheConference(models.Model):
+   
+    description = models.TextField(default = '')
+    link = models.CharField(max_length = 100,default = '',null= True,blank = True)
+
+    def __str__(self):
+        return str(self.description)
+
+class CallForPaper(models.Model):
+   
+    description = models.TextField(default = '')
+
+    def __str__(self):
+        return str(self.description)
+
+
+class ImportantDate(models.Model):
+
+    description = models.TextField(default = '')
+    date = models.TextField(default = '')
+    highlighted = models.TextField(default = '')
+    
+    def __str__(self):
+        return str(self.date)
+
+class Speaker(models.Model):
+   
+    name = models.CharField(default = '', max_length=50)
+
+    def __str__(self):
+        return str(self.name)
+
+class Organiser(models.Model):
+   
+    name = models.CharField(default = '', max_length=50)
+
+    def __str__(self):
+        return str(self.name)
+
+class Contact(models.Model):
+   
+    description = models.TextField(default = '')
+
+    def __str__(self):
+        return str(self.description)
+
+class RegistrationContact(models.Model):
+
+    description = models.TextField(default = '')
+    link = models.CharField(max_length = 100,default = '',null= True,blank = True)
+
+    def __str__(self):
+        return str(self.description)
+
+class Announcement(models.Model):
+
+    description = models.TextField(default = '')
+    title = models.CharField(max_length = 100,default = '')
+    highlights = models.TextField(default = '')
+
+    def __str__(self):
+        return str(self.title)
+
+class BankDetail(models.Model):
+
+    description = models.TextField(default = '')
+
+    def __str__(self):
+        return str(self.description)
+
+class RegistrationFee(models.Model):
+
+    description = models.TextField(default = '')
+
+    def __str__(self):
+        return str(self.description)
+
+class AccomodationAndTravel(models.Model):
+
+    description = models.TextField(default = '')
+
+    def __str__(self):
+        return str(self.description)
+
+
+
--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 
 from django.urls import path,include
-from .views import PostCreateView,PostDeleteView,PostUpdateView,HomePage,RegistrationPage,TravelPage,TaskPage,ConferencePage,PosterPage
+from .views import PostCreateView,PostDeleteView,PostUpdateView,FeesDeleteView,FeesCreateView,FeesUpdateView,AnnouncementsUpdateView,AnnouncementsCreateView,AnnouncementsDeleteView,HomePage,RegistrationPage,TravelPage,BankDeleteView, BankCreateView, BankUpdateView,TaskPage,ConferencePage,PosterPage,AccomodationUpdateView,RContactUpdateView,OrganisersCreateView,OrganisersDeleteView,OrganisersUpdateView, AboutUpdateView,ContactUpdateView, PapersUpdateView, DatesUpdateView, DatesDeleteView, DatesCreateView, SpeakersCreateView, SpeakersUpdateView, SpeakersDeleteView
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
@@ -8,14 +8,34 @@ urlpatterns = [
    path('edit/<int:id>',PostUpdateView, name = 'update-Post'),
    path('login',auth_views.LoginView.as_view(template_name = 'blog/login.html'),name = 'login'),
    path('logout',auth_views.LogoutView.as_view(template_name = 'blog/logout.html'),name = 'logout'),
-      path('',HomePage, name = 'home'),
-      path('task-schedule',TaskPage, name = 'task-schedule'),
-      path('registration/',RegistrationPage, name = 'registration'),
-      path('travel-and-accomodation',TravelPage, name = 'travel'),
-      path('delete/<int:id>',PostDeleteView, name = 'delete-Post'),
-      path('conference-preceedings/',ConferencePage,name = 'conference'),
-      path('poster',PosterPage,name ='poster'),
-
-    
-
+   path('',HomePage, name = 'home'),
+   path('task-schedule',TaskPage, name = 'task-schedule'),
+   path('registration/',RegistrationPage, name = 'registration'),
+   path('travel-and-accomodation/',TravelPage, name = 'travel'),
+   path('delete/<int:id>',PostDeleteView, name = 'delete-Post'),
+   path('conference-preceedings/',ConferencePage,name = 'conference'),
+   path('poster',PosterPage,name ='poster'),
+   path('edit/about-the-conference/<int:pk>',AboutUpdateView.as_view(), name = 'update-about'),
+   path('edit/call-for-papers/<int:pk>',PapersUpdateView.as_view(), name = 'update-papers'),
+   path('edit/accomodation-and-travel/<int:pk>',AccomodationUpdateView.as_view(), name = 'update-accomodation'),
+   path('edit/registration-contact/<int:pk>',RContactUpdateView.as_view(), name = 'update-reg-contact'),
+   path('create/announcement/',AnnouncementsCreateView.as_view(), name = 'add-announcement'),
+   path('delete/announcement/<int:pk>',AnnouncementsDeleteView.as_view(), name = 'delete-announcement'),
+   path('edit/announcement/<int:pk>',AnnouncementsUpdateView.as_view(), name = 'update-announcement'),
+   path('edit/important-dates/<int:pk>',DatesUpdateView.as_view(), name = 'update-dates'),
+   path('delete/important-dates/<int:pk>',DatesDeleteView.as_view(), name = 'delete-dates'),
+   path('create/important-dates/',DatesCreateView.as_view(), name = 'new-dates'),
+   path('edit/speakers/<int:pk>',SpeakersUpdateView.as_view(), name = 'update-speaker'),
+   path('delete/speakers/<int:pk>',SpeakersDeleteView.as_view(), name = 'delete-speaker'),
+   path('create/speakers/',SpeakersCreateView.as_view(), name = 'new-speaker'),
+   path('edit/organisers/<int:pk>',OrganisersUpdateView.as_view(), name = 'update-organiser'),
+   path('delete/organisers/<int:pk>',OrganisersDeleteView.as_view(), name = 'delete-organiser'),
+   path('create/organisers/',OrganisersCreateView.as_view(), name = 'new-organiser'),
+   path('edit/contact/<int:pk>',ContactUpdateView.as_view(), name = 'update-contact'),
+   path('delete/bank-detail/<int:pk>',BankDeleteView.as_view(), name = 'delete-bank-detail'),
+   path('new/bank-detail/',BankCreateView.as_view(), name = 'new-bank-detail'),
+   path('edit/bank-detail/<int:pk>',BankUpdateView.as_view(), name = 'update-bank-detail'),
+   path('delete/fee-details/<int:pk>',FeesDeleteView.as_view(), name = 'delete-fees'),
+   path('new/fee-details/',FeesCreateView.as_view(), name = 'new-fees'),
+   path('edit/fee-details/<int:pk>',FeesUpdateView.as_view(), name = 'update-fees'),
 ]
